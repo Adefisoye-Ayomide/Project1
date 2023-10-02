f3240ee92b60cd149f0e9d7b719648252ee6fa56
Ayomide Matthew Adefisoye <57393575+Adefisoye-Ayomide@users.noreply.github.com>
2023-10-02 03:05:38 -0400

    Update euler_method.py

--- euler_method.py
+++ euler_method.py
@@ -1,13 +1,21 @@
 import numpy as np
-from viscous_decay import Viscous
+
+# Define the ODE function: dv/dt = -v/tau
+def viscous_decay(v, t, tau):
+    dvdt = -v / tau
+    return dvdt
 
 class Euler_method:
-# Euler's method
-    def calcuate_euler_method(v0, t_span, tau):
+    # Euler's method
+    def calculate_euler_method(self, v0, t_span, tau):
         v = np.zeros(len(t_span))  # Initialize an array for velocity values
         v[0] = v0  # Set the initial value
         dt = t_span[1] - t_span[0]  # Time step
         for i in range(1, len(t_span)):
-            dvdt = Viscous(v[i - 1], t_span[i - 1], tau)
+            dvdt = viscous_decay(v[i - 1], t_span[i - 1], tau)
             v[i] = v[i - 1] + dvdt * dt
         return v
+
+    
+
+
